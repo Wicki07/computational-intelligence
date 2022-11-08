39e7022ad0bb82786de41bcb76ee669be30d403d
Mateusz Wicki <mateuszwicki1@gmail.com>
2022-11-08 19:24:18 +0100

    Lab5

---
+++ Lab5/zad2.py
@@ -0,0 +1,29 @@
+from simpful import *
+
+FS = FuzzySystem()
+
+TLV = AutoTriangle(3, terms=['low', 'medium', 'high'], universe_of_discourse=[0,1])
+FS.add_linguistic_variable("distance", TLV)
+FS.add_linguistic_variable("speed", TLV)
+
+O1 = TriangleFuzzySet(0,0,16,   term="low")
+O2 = TriangleFuzzySet(0,16,30,  term="medium")
+O3 = TriangleFuzzySet(16,30,30, term="high")
+FS.add_linguistic_variable("tip", LinguisticVariable([O1, O2, O3], universe_of_discourse=[0,30]))
+
+FS.add_rules([
+	"IF (quality IS poor) OR (service IS poor) THEN (tip IS low)",
+	"IF (service IS average) THEN (tip IS medium)",
+	"IF (quality IS good) OR (quality IS good) THEN (tip IS high)"
+	])
+
+FS.set_variable("quality", 3)
+FS.set_variable("service", 8)
+
+tipMamdani = FS.Mamdani_inference()
+
+
+print(tipMamdani)
+FS.plot_variable("quality")
+FS.plot_variable("service")
+FS.plot_variable("tip")
